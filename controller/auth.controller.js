c57528479035ae1f17356c631d7ca5e783b4ac86
DevrajPun <devrajpunthapa@gmail.com>
2024-09-27 00:08:33 +0530

    first commit

---
+++ controller/auth.controller.js
@@ -0,0 +1,82 @@
+
+import user from "../models/user.js"
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+export const signup = async (req, res, next) => {
+
+    const { username, email, password } = req.body
+
+    const isValidUser = await user.findOne({ email })
+
+    if (isValidUser) {
+
+        return next(errorHandler(404, "User already Exist"))
+
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new user({
+        username,
+        email,
+        password: hashedPassword
+    })
+    try {
+        await newUser.save()
+        res.status(201).json({
+            success: true,
+            message: "User Created Successfully"
+        })
+
+    } catch (error) {
+        next(error)
+
+    }
+}
+
+export const signin = async (req, res, next) => {
+
+    const { email, password } = req.body
+
+    try {
+        const ValidUser = await user.findOne({ email })
+
+        if (!ValidUser) {
+            return next(errorHandler(404, "user not found"))
+        }
+        const validPassword = bcryptjs.compareSync(password, ValidUser.password)
+
+        if (!validPassword) {
+
+            return next(errorHandler(401, "wrong credentials"))
+        }
+
+        const token = jwt.sign({ id: ValidUser._id }, process.env.JWT_SECRET)
+
+        const { password: pass, ...rest } = ValidUser._doc
+
+        res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+            success: true,
+            message: "Login Successfull",
+            rest,
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const signout = async (req, res, next) => {
+    try {
+
+        res.clearCookie("access_token")
+
+        res.status(200).json({
+            success: true,
+            message: "User logged out successfully",
+        })
+    } catch (error) {
+        next(error)
+    }
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+import express, { urlencoded } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("connected to mongoDB")
+}).catch((error) => {
+  console.log(error)
+})
+
+
+const app = express();
+
+//to make input as json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
+
+// input routes
+import authRouter from "./routes/auth.route.js"
+
+import noteRouter from "./routes/note.route.js"
+
+app.use("/api/auth", authRouter)
+app.use("/api/note", noteRouter)
+
+
+//error handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Server Error"
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
